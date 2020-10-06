135b1765d8d55bcdd3ee634646e4b429fc4ccaad
Richard Shirley <56078639+EternalDachirr@users.noreply.github.com>
2020-10-06 17:27:34 -0400

    Add files via upload

---
+++ javaPattern.java
@@ -0,0 +1,9 @@
+public class javaPattern {
+  public static void main(String[] args) {
+    //I am using stacked print statements to display the desired pattern.
+    System.out.println("    J    A     V    V   A");
+    System.out.println("    J   A A     V  V   A A");
+    System.out.println("J   J  AAAAA     V V  AAAAA");
+    System.out.println(" J J  A     A     V  A     A");
+  }
+}
